e58fe12ae9caab8acd8d954971ccb4654fe303f4
Ahmed Mathlouthi <ahmed.mathlouhthi.am@gmail.com>
2023-07-04 22:20:31 +0200

    add validation

---
+++ evaluation.rb
@@ -0,0 +1,54 @@
+class Evaluation
+    
+    attr_accessor :type, :value, :score, :state, :reason
+  
+    TYPES = %w[SIREN].freeze
+    STATES = %w[favorable unfavorable unconfirmed].freeze
+    REASONS = %w[unable_to_reach_api company_closed ongoing_database_update company_opened].freeze
+  
+    def initialize(type:, value:, score:, state:, reason:)
+      @type = validate_type(type)
+      @value = value
+      @score = validate_score(score)
+      @state = validate_state(state)
+      @reason = validate_reason(reason)
+    end
+  
+    def to_s()
+      "#{@type}, #{@value}, #{@score}, #{@state}, #{@reason}"
+    end
+  
+    def validate_score(score)
+      if score < 0
+        raise StandardError => e
+          e.message = 'cant go below 0'
+      end
+
+      score
+    end
+
+    def validate_type(type)
+      unless TYPES.include? type
+        raise StandardError.new("type must be in #{TYPES.join(',')}")
+      end
+
+      type
+    end
+
+    def validate_state(state)
+      unless STATES.include? state
+        raise StandardError.new("state must be in #{STATES.join(',')}")
+      end
+
+      state
+    end
+
+    def validate_reason(reason)
+      unless REASONS.include? reason
+        raise StandardError.new("reason must be in #{REASONS.join(',')}")
+      end
+
+      reason
+    end
+  end
+  
--- trustin.rb
+++ trustin.rb
@@ -1,6 +1,7 @@
 require "json"
 require "net/http"
 require 'pry'
+# require './evaluation'
 
 class TrustIn
   def initialize(evaluations)
@@ -9,10 +10,46 @@ class TrustIn
 
   def update_score
     @evaluations.each do |evaluation|
-      next unless evaluation.type == "SIREN"
 
-      if evaluation.score > 0 && evaluation.state == "unconfirmed" && evaluation.reason == "ongoing_database_update"
+      if evaluation.type == "SIREN"
+        update_siren_companies(evaluation)
+      end
+
+      if evaluation.type == 'VAT'
+        update_vat_companies(evaluation)
+      end
+    end
+  end
+
+  def update_siren_companies(evaluation)
+    if evaluation.score > 0 && evaluation.state == "unconfirmed" && evaluation.reason == "ongoing_database_update"
+      parsed_response = api_response(evaluation.value)
+
+      company_state = parsed_response["records"]&.first&.dig('fields')&.dig("etatadministratifetablissement")
+      
+      if company_state == "Actif"
+        evaluation.state = "favorable"
+        evaluation.reason = "company_opened"
+        evaluation.score = 100
+      else
+        evaluation.state = "unfavorable"
+        evaluation.reason = "company_closed"
+        evaluation.score = 100
+      end
+    elsif evaluation.score >= 50
+      if evaluation.state == "unconfirmed" && evaluation.reason == "unable_to_reach_api"
+        evaluation.score -= 5
+      elsif evaluation.state == "favorable"
+        evaluation.score -= 1
+      end
+    elsif evaluation.score <= 50 && evaluation.score > 0 
+      if evaluation.state == "unconfirmed" && evaluation.reason == "unable_to_reach_api" || evaluation.state == "favorable"
+        evaluation.score -= 1
+      end
+    else
+      if evaluation.state == "favorable" || evaluation.state == "unconfirmed"
         parsed_response = api_response(evaluation.value)
+        return unless parsed_response
 
         company_state = parsed_response["records"]&.first&.dig('fields')&.dig("etatadministratifetablissement")
         
@@ -25,36 +62,19 @@ class TrustIn
           evaluation.reason = "company_closed"
           evaluation.score = 100
         end
-      elsif evaluation.score >= 50
-        if evaluation.state == "unconfirmed" && evaluation.reason == "unable_to_reach_api"
-          evaluation.score -= 5
-        elsif evaluation.state == "favorable"
-          evaluation.score -= 1
-        end
-      elsif evaluation.score <= 50 && evaluation.score > 0 
-        if evaluation.state == "unconfirmed" && evaluation.reason == "unable_to_reach_api" || evaluation.state == "favorable"
-          evaluation.score -= 1
-        end
-      else
-
-        if evaluation.state == "favorable" || evaluation.state == "unconfirmed"
-          parsed_response = api_response(evaluation.value)
-          next unless parsed_response
-
-          company_state = parsed_response["records"]&.first&.dig('fields')&.dig("etatadministratifetablissement")
-          
-          if company_state == "Actif"
-            evaluation.state = "favorable"
-            evaluation.reason = "company_opened"
-            evaluation.score = 100
-          else
-            evaluation.state = "unfavorable"
-            evaluation.reason = "company_closed"
-            evaluation.score = 100
-          end
-        end
       end
     end
+  end
+
+  def update_vat_companies(evaluation)
+    company_state = parsed_response["records"]&.first&.dig('fields')&.dig("etatadministratifetablissement")
+
+    if evaluation.score >= 50 
+      evaluation.value -= 1
+    else
+      evaluation.value -= 3
+    end
+
   end
 
   def api_response(value)
@@ -64,7 +84,8 @@ class TrustIn
     response = Net::HTTP.get(uri)
     parsed_response = JSON.parse(response)
 
-    raise UndefinedSirenError.new(value) unless parsed_response.dig('error').nil?
+  # rescue StandardError
+  #   puts 'unable to reach Api'
   end
 end
 
@@ -79,46 +100,3 @@ class UndefinedSirenError < StandardError
   end
 end
 
-class Evaluation
-  attr_accessor :type, :value, :score, :state, :reason
-
-  TYPES = %w[SIREN].freeze
-  STATES = %w[favorable unfavorable unconfirmed].freeze
-  REASONS = %w[unable_to_reach_api company_closed ongoing_database_update company_opened].freeze
-
-  def initialize(type:, value:, score:, state:, reason:)
-    @type = type
-    @value = value
-    @score = score
-    @state = state
-    @reason = reason
-  end
-
-  def to_s()
-    "#{@type}, #{@value}, #{@score}, #{@state}, #{@reason}"
-  end
-
-  # def state=(str)
-  #   @state = STATES.index(str)
-  # end
-
-  # def state
-  #   STATES[@state]
-  # end
-
-  # def type=(str)
-  #   @type = TYPES.index(str)
-  # end
-
-  # def type
-  #   TYPES[@type]
-  # end
-
-  # def reason=(str)
-  #   @reason = REASONS.index(str)
-  # end
-
-  # def reason
-  #   REASONS[@reason]
-  # end
-end
